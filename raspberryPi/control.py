95499ee759cc5a135c22840e6f8d9efc4c3e3481
djvolz <volz.developer@gmail.com>
2016-11-14 18:55:27 -0800

    Factory set up and ready to dazzle
    code cleanup for development (going to be a lot of rewrites at these early stages)

--- raspberryPi/control.py
+++ raspberryPi/control.py
@@ -2,10 +2,10 @@
 # @Author: djvolz
 # @Date:   2016-11-14 17:03:11
 # @Last Modified by:   djvolz
-# @Last Modified time: 2016-11-14 17:47:13
+# @Last Modified time: 2016-11-14 18:53:59
 
 import time
-import json  # import json library to parse messages
+# import json  # import json library to parse messages
 import boto3  # import boto library that handles queuing functions
 import lightSequence as ls
 
@@ -23,18 +23,32 @@ class Controller:
     # print(response.get('MessageId'))
     # print(response.get('MD5OfMessageBody'))
 
-    def lightsFactory(self, scheme):
-        program = ls.LightSequence.factory('Chase')
+    def lightsFactory(self, type):
+        program = ls.LightSequence.factory(type)
         while True:
             program.run()
 
     def processMessages(self):
         # Create a new message for testing
-        response = self._queue.send_message(MessageBody='world')
+        # response = self._queue.send_message(MessageBody='world')
 
         while True:
             # Process messages by printing out body and optional author name
-            for message in self._queue.receive_messages(MessageAttributeNames=['Author']):
+            for message in \
+                    self._queue.receive_messages(
+                    MessageAttributeNames=['Author']):
+
+                print(message)
+
+                # TODO: process the message and respond with appropriate action
+                # msg = json.loads(incomingMsg.get_body())
+                # action = message['request']['action']
+
+                # if action == 'lights on':
+                #     print 'TURNING LIGHTS ON LIKE WE AIN'T GOT NO POWER BILL'
+                # if this works then:
+                # lightsFactory(action)
+
                 # Get the custom author message attribute if it was set
                 author_text = ''
                 if message.message_attributes is not None:
@@ -53,44 +67,4 @@ class Controller:
 
     def run(self):
         # Jump straight in
-        # self.processMessages()
-
-        # for i in range(1,1000):
-        #     # check queue to see if a request exists
-        #     incomingMsgs = queue.get_messages()
-
-        #     # if messages are found, process
-        #     if len(incomingMsgs) > 0:
-        #         for incomingMsg in incomingMsgs:
-        #             msg = json.loads(incomingMsg.get_body())
-        #             action = msg['request']['action']
-
-        #       # check what the requested action is
-        #       if action == 'lights on':
-        #           print 'pitch the ball'
-
-        #           # set parameters for IO on raspberry PI
-        #               # relay_pin = 12
-
-        #           # configure IO on raspberry PI to communicate
-        #           # GPIO.setmode(GPIO.BOARD)
-        #           # GPIO.setwarnings(False)
-        #           # GPIO.setup(relay_pin, GPIO.OUT)
-
-        #           print "Relay Active"
-
-        #           # GPIO.output(relay_pin, GPIO.HIGH)
-
-        #           # pause
-        #           time.sleep(rotor_duration)
-
-        #       print "Relay Off"
-
-        #           # GPIO.output(relay_pin, GPIO.LOW)
-
-        #           #remove message from queue
-        #           queue.delete_message(incomingMsg)
-
-        #           # GPIO.cleanup()
-
-        #     print i
+        self.processMessages()
--- raspberryPi/lightSequence.py
+++ raspberryPi/lightSequence.py
@@ -2,7 +2,7 @@
 # @Author: djvolz
 # @Date:   2016-11-14 17:04:42
 # @Last Modified by:   djvolz
-# @Last Modified time: 2016-11-14 17:31:13
+# @Last Modified time: 2016-11-14 18:00:57
 
 import chase
 
@@ -12,9 +12,10 @@ numLEDs = 64
 
 class LightSequence(object):
     # Create based on class name:
+    @staticmethod
     def factory(type):
         # return eval(type + "()")
+        print("Generating " + type)
         if type == "Chase":
             return chase.Chase(client, numLEDs)
         assert 0, "Bad light sequence creation: " + type
-    factory = staticmethod(factory)
